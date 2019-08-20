02a352a72192f0f1b78e12e06fff6d013ea5b874
Vincent den Boer <vincent@shishkabab.net>
2019-08-20 15:54:12 +0200

    More progress on Sync tests

--- src/custom-lists/background/storage.ts
+++ src/custom-lists/background/storage.ts
@@ -117,10 +117,7 @@ export default class CustomListStorage extends StorageModule {
                         id: '$id:pk',
                     },
                     {
-                        $set: {
-                            name: '$name:string',
-                            createdAt: new Date(),
-                        },
+                        name: '$name:string',
                     },
                 ],
             },
