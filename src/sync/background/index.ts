0078041c03ff47441e14a933cc4b3a21a7470416
Vincent den Boer <vincent@shishkabab.net>
2020-01-28 15:32:41 +0100

    Further refactoring and fix fav icon saving

--- src/sync/background/index.ts
+++ src/sync/background/index.ts
@@ -20,6 +20,7 @@ import { INCREMENTAL_SYNC_FREQUENCY } from './constants'
 import { filterBlobsFromSyncLog } from './sync-logging'
 import { MemexExtSyncSettingStore } from './setting-store'
 import { resolvablePromise } from 'src/util/promises'
+import { bindMethod } from 'src/util/functions'
 
 export default class SyncBackground extends SyncService {
     initialSync: MemexInitialSync
@@ -60,29 +61,33 @@ export default class SyncBackground extends SyncService {
 
         this.auth = options.auth
 
-        const bound = <Target, Key extends keyof Target>(
-            object: Target,
-            key: Key,
-        ): Target[Key] => (object[key] as any).bind(object)
-
         this.remoteFunctions = {
-            requestInitialSync: bound(this.initialSync, 'requestInitialSync'),
-            answerInitialSync: bound(this.initialSync, 'answerInitialSync'),
-            waitForInitialSync: bound(this.initialSync, 'waitForInitialSync'),
-            waitForInitialSyncConnected: bound(
+            requestInitialSync: bindMethod(
+                this.initialSync,
+                'requestInitialSync',
+            ),
+            answerInitialSync: bindMethod(
+                this.initialSync,
+                'answerInitialSync',
+            ),
+            waitForInitialSync: bindMethod(
+                this.initialSync,
+                'waitForInitialSync',
+            ),
+            waitForInitialSyncConnected: bindMethod(
                 this.initialSync,
                 'waitForInitialSyncConnected',
             ),
-            enableContinuousSync: bound(
+            enableContinuousSync: bindMethod(
                 this.continuousSync,
                 'enableContinuousSync',
             ),
-            forceIncrementalSync: bound(
+            forceIncrementalSync: bindMethod(
                 this.continuousSync,
                 'forceIncrementalSync',
             ) as () => Promise<void>,
-            listDevices: bound(this.syncInfoStorage, 'listDevices'),
-            removeDevice: bound(this.syncInfoStorage, 'removeDevice'),
+            listDevices: bindMethod(this.syncInfoStorage, 'listDevices'),
+            removeDevice: bindMethod(this.syncInfoStorage, 'removeDevice'),
         }
     }
 
